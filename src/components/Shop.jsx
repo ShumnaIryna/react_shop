a6ffcabcb5d2de3605a654ae435cb2f3a63e4ae7
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-05-26 15:56:54 +0300

    added new components

---
+++ src/components/Shop.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { API_URL } from "../config";
+
+import { Preloader } from "./Preloader";
+import { GoodsList } from "./GoodsList";
+
+//const API_KEY = process.env.REACT_APP_API_KEY;
+
+const API_KEY = `548fddbc-b16a4210-9bb19577-b682b006`;
+
+function Shop() {
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function getGoods() {
+    fetch(API_URL, {
+      // method: "GET",
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.featured && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className="container content">
+      {loading ? <Preloader /> : <GoodsList goods={goods} />}
+    </main>
+  );
+}
+
+export { Shop };
---
+++ src/config.js
@@ -0,0 +1,6 @@
+//const API_KEY = "548fddbc-b16a4210-9bb19577-b682b006";
+//const API_KEY = process.env.REACT_APP_API_KEY;
+
+const API_URL = "https://fortniteapi.io/v1/shop?lang=en";
+
+export { API_URL };
