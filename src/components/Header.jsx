0e1143c51de3ce23c1c91604b93c80e2a6626f6a
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-06-08 13:10:51 +0300

    added minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,17 @@ function Header() {
   return (
     <nav className="#ff8a65 deep-orange lighten-2">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a
+              href="https://shumnairyna.github.io/react_shop/"
+              target="_blank"
+              rel="noreferrer">
+              Repo
+            </a>
           </li>
         </ul>
       </div>
